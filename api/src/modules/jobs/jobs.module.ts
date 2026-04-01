0a93efc659c9c168b6d0c528bd3e12ecd4c694e8
Anthony DePaolo <adepaolo456@gmail.com>
2026-03-31 23:29:04 -0400

    refactor: consolidate all invoice creation through BillingService

--- api/src/modules/jobs/jobs.module.ts
+++ api/src/modules/jobs/jobs.module.ts
@@ -2,15 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Job } from './entities/job.entity';
 import { Asset } from '../assets/entities/asset.entity';
-import { Invoice } from '../billing/entities/invoice.entity';
 import { PricingRule } from '../pricing/entities/pricing-rule.entity';
 import { AutomationLog } from '../automation/entities/automation-log.entity';
 import { Customer } from '../customers/entities/customer.entity';
+import { BillingModule } from '../billing/billing.module';
 import { JobsService } from './jobs.service';
 import { JobsController } from './jobs.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Job, Asset, Invoice, PricingRule, AutomationLog, Customer])],
+  imports: [TypeOrmModule.forFeature([Job, Asset, PricingRule, AutomationLog, Customer]), BillingModule],
   controllers: [JobsController],
   providers: [JobsService],
   exports: [JobsService],
